af3aac85b55d9849f180ab2e4d2678ffd39d94bf
hongkuan <442724184@qq.com>
2021-03-15 16:05:27 -0700

    sanity check failed? predict only history events gives 68 filtered MRR on WIKI?

---
+++ check_test_event_sanity.py
@@ -0,0 +1,15 @@
+from events import Events
+import colorama
+
+for d in ['GDELT', 'ICEWS14', 'ICEWS18', 'WIKI', 'YAGO']:
+    e = Events(d)
+    e.update_copy_mask(e.ts_train + e.ts_val)
+    hist = e.object_copy_mask_dict
+    total = 0
+    hit = 0
+    for ev in e.test_events:
+        for s, r, o, _ in ev:
+            total += 1
+            if o in hist[r][s]:
+                hit += 1
+    print(colorama.Fore.RED + '{}\thit%: {:4f}\ttotal:{:d}'.format(d, hit / total, total) + colorama.Style.RESET_ALL)
