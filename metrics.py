742a57db5d8aa6ebcfc5bc765629c6e44e0d333f
hongkuan <442724184@qq.com>
2021-03-17 18:53:36 -0700

    ..

--- metrics.py
+++ metrics.py
@@ -9,7 +9,7 @@ def mrr(rank):
     return torch.mean(torch.reciprocal(rank.type(torch.float)))
 
 def hit3(rank):
-    return torch.mean((rank < 3).type(torch.float))
+    return torch.mean((rank <= 3).type(torch.float))
 
 def hit10(rank):
-    return torch.mean((rank < 10).type(torch.float))
+    return torch.mean((rank <= 10).type(torch.float))
